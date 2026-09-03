8e0e876f7b1764133c22079369ebc00505a0d3cb
sathindu123 <sathindusathsarakumara@gmail.com>
2026-09-03 13:35:59 +0530

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     apps: [
         {
+            name: "cloud-sql-auth-proxy",
+            script: "./cloud-sql-proxy sathindu-gcp-lab:asia-southeast1:postgres-vm sathindu-gcp-lab:asia-southeast1:mysql-vm --private-ip",
+            log_file: "./logs/cloud-sql-auth-proxy.log"
+        },
+        {
             name: "student-service",
             script: "java -jar student-service/target/Student-Service-1.0.0.jar",
             log_file: "./logs/student-service.log",
